61ec29380c96c7f91956210eb95d8d6daba5535f
aniretakey <EMKornishina@gmail.com>
2023-04-07 09:01:41 +0300

    fix: update burger menu animation

--- shelter/javascript/burger.js
+++ shelter/javascript/burger.js
@@ -1,16 +1,29 @@
 const iconMenu = document.querySelector(".hamburger");
 const nav = document.querySelector(".header__navigation");
+const overlay = document.querySelector(".overlay");
+
+function overlayAdd() {
+  overlay.classList.add("overlay_active");
+}
+
 if (iconMenu) {
-  iconMenu.addEventListener("click", function () {
+  iconMenu.addEventListener("click", function (e) {
+    // window.setTimeout(overlayAdd, 600);
     document.body.classList.toggle("lock");
     iconMenu.classList.toggle("_active");
     nav.classList.toggle("header__navigation_active");
+    if (iconMenu.classList.contains("_active")) {
+      window.setTimeout(overlayAdd, 650);
+    } else {
+      overlay.classList.remove("overlay_active");
+    }
   });
 }
 
-if (nav) {
-  nav.addEventListener("click", function () {
+if (overlay) {
+  overlay.addEventListener("click", function (e) {
     if (iconMenu.classList.contains("_active")) {
+      overlay.classList.remove("overlay_active");
       document.body.classList.remove("lock");
       iconMenu.classList.remove("_active");
       nav.classList.remove("header__navigation_active");
